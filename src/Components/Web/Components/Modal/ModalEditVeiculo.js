60b08b6d343b16fed1913ac4787aaed3fe75226a
Mateus Barreto <130594274+mateusbarret0@users.noreply.github.com>
2024-12-05 13:06:17 -0300

    fix: edit veiculo

--- src/Components/Web/Components/Modal/ModalEditVeiculo.js
+++ src/Components/Web/Components/Modal/ModalEditVeiculo.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import ModalStyle from "../Modal/ModalStyle";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import React, { useState, useEffect } from 'react';
+import ModalStyle from '../Modal/ModalStyle';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import {
   Button,
   FormControl,
@@ -9,30 +9,33 @@ import {
   MenuItem,
   Select,
   TextField,
-} from "@mui/material";
-import ClearIcon from "@mui/icons-material/Clear";
-import CheckIcon from "@mui/icons-material/Check";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { EDIT_VEICULOS, GET_MOTORISTAS } from "../../../../api";
-import { toast } from "react-toastify";
-import InputDate from "../Input/InputDate";
-import dayjs from "dayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+} from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import CheckIcon from '@mui/icons-material/Check';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { EDIT_VEICULOS, GET_MOTORISTAS } from '../../../../api';
+import { toast } from 'react-toastify';
+import InputDate from '../Input/InputDate';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
+  console.log('🚀 - ModalEditVeiculo - data:', data);
   const [loading, setLoading] = useState(false);
-  const [numVeiculo, setNumVeiculo] = useState("");
-  const [modelo, setModelo] = useState("");
-  const [placa, setPlaca] = useState("");
-  const [ano, setAno] = useState("");
-  const [capacidade, setCapacidade] = useState("");
-  const [dataProxManutencao, setDataProxManutencao] = useState("");
-  const [dataUltManutencao, setDataUltManutencao] = useState("");
-  const [empresa, setEmpresa] = useState("");
-  const [motorista, setMotorista] = useState("");
+  const [numVeiculo, setNumVeiculo] = useState('');
+  const [modelo, setModelo] = useState('');
+  const [placa, setPlaca] = useState('');
+  const [ano, setAno] = useState('');
+  const [capacidade, setCapacidade] = useState('');
+  const [dataProxManutencao, setDataProxManutencao] = useState('');
+  const [dataUltManutencao, setDataUltManutencao] = useState('');
+  const [empresa, setEmpresa] = useState('');
+  const [motorista, setMotorista] = useState('');
+  console.log('🚀 - ModalEditVeiculo - motorista:', motorista);
   const [selectMotoristas, setSelectMotoristas] = useState([]);
-  const [tipoVeiculo, setTipoVeiculo] = useState("");
+  console.log('🚀 - ModalEditVeiculo - selectMotoristas:', selectMotoristas);
+  const [tipoVeiculo, setTipoVeiculo] = useState('');
 
   useEffect(() => {
     if (data) {
@@ -44,21 +47,21 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
       setDataProxManutencao(data?.dt_prox_manu);
       setDataUltManutencao(data?.dt_ultim_manu);
       setEmpresa(data?.empresa);
-      setMotorista(data?.motorista);
+      setMotorista(data?.cod_motorista);
       setTipoVeiculo(data?.tipo_veiculo);
     }
   }, [data]);
 
   const darkTheme = createTheme({
     palette: {
-      mode: "dark",
+      mode: 'dark',
       background: {
-        default: "#121212",
-        paper: "#192038",
+        default: '#121212',
+        paper: '#192038',
       },
       text: {
-        primary: "#FFFFFF",
-        secondary: "#B0B0B0",
+        primary: '#FFFFFF',
+        secondary: '#B0B0B0',
       },
     },
   });
@@ -86,11 +89,11 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
         toast.success(`Veículo ${data.placa} autalizado com sucesso!`);
         close();
       } else {
-        toast.error("Erro ao atualizar o veículo");
-        console.log("Erro ao atualizar o veículo:", json);
+        toast.error('Erro ao atualizar o veículo');
+        console.log('Erro ao atualizar o veículo:', json);
       }
     } catch (error) {
-      console.error("Erro na requisição:", error);
+      console.error('Erro na requisição:', error);
     } finally {
       setLoading(false);
     }
@@ -102,7 +105,7 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
     const json = await response.json();
     console.log(json);
     if (response.ok) setSelectMotoristas(json);
-    else console.log("Erro ao buscar motoristas:", json);
+    else console.log('Erro ao buscar motoristas:', json);
   };
 
   useEffect(() => {
@@ -116,12 +119,12 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
         close={close}
         title={
           <>
-            <Box sx={{ display: "flex" }}>
+            <Box sx={{ display: 'flex' }}>
               <Typography
                 sx={{
                   fontSize: 25,
-                  fontWeight: "700",
-                  color: "white",
+                  fontWeight: '700',
+                  color: 'white',
                   mr: 42,
                 }}
               >
@@ -131,7 +134,7 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
               <FormControl sx={{ width: 200 }}>
                 <InputLabel
                   id="demo-simple-select-label"
-                  sx={{ color: "#FFFFFF" }}
+                  sx={{ color: '#FFFFFF' }}
                 >
                   Tipo de Veículo
                 </InputLabel>
@@ -141,8 +144,8 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                   value={tipoVeiculo}
                   onChange={(e) => setTipoVeiculo(e.target.value)}
                   sx={{
-                    color: "#FFFFFF",
-                    backgroundColor: "#192038",
+                    color: '#FFFFFF',
+                    backgroundColor: '#192038',
                     borderRadius: 3,
                   }}
                 >
@@ -156,15 +159,15 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
         color={color}
         content={
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Box sx={{ width: "100%", height: "100%" }}>
-              <Box sx={{ width: "100%", display: "flex", gap: 2, mb: 2 }}>
+            <Box sx={{ width: '100%', height: '100%' }}>
+              <Box sx={{ width: '100%', display: 'flex', gap: 2, mb: 2 }}>
                 <ThemeProvider theme={darkTheme}>
                   <TextField
                     sx={{
-                      backgroundColor: "#192038",
+                      backgroundColor: '#192038',
                       borderRadius: 3,
-                      width: "50%",
-                      fontSize: "1rem",
+                      width: '50%',
+                      fontSize: '1rem',
                     }}
                     id="modelo"
                     label="Modelo do veículo"
@@ -174,10 +177,10 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                   />
                   <TextField
                     sx={{
-                      backgroundColor: "#192038",
+                      backgroundColor: '#192038',
                       borderRadius: 3,
-                      width: "25%",
-                      fontSize: "1rem",
+                      width: '25%',
+                      fontSize: '1rem',
                     }}
                     id="placa"
                     label="Placa"
@@ -187,10 +190,10 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                   />
                   <TextField
                     sx={{
-                      backgroundColor: "#192038",
+                      backgroundColor: '#192038',
                       borderRadius: 3,
-                      width: "25%",
-                      fontSize: "1rem",
+                      width: '25%',
+                      fontSize: '1rem',
                     }}
                     id="ano"
                     label="Ano"
@@ -202,14 +205,14 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                 </ThemeProvider>
               </Box>
 
-              <Box sx={{ width: "100%", display: "flex", gap: 2, mb: 2 }}>
+              <Box sx={{ width: '100%', display: 'flex', gap: 2, mb: 2 }}>
                 <ThemeProvider theme={darkTheme}>
                   <TextField
                     sx={{
-                      backgroundColor: "#192038",
+                      backgroundColor: '#192038',
                       borderRadius: 3,
-                      width: "33%",
-                      fontSize: "1rem",
+                      width: '33%',
+                      fontSize: '1rem',
                     }}
                     id="capacidade"
                     label="Capacidade"
@@ -223,10 +226,10 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                     setValue={setDataUltManutencao}
                     label="Data da última manutenção"
                     sx={{
-                      backgroundColor: "#192038",
+                      backgroundColor: '#192038',
                       borderRadius: 3,
-                      width: "33%",
-                      fontSize: "1rem",
+                      width: '33%',
+                      fontSize: '1rem',
                     }}
                   />
                   <InputDate
@@ -234,23 +237,23 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                     setValue={setDataProxManutencao}
                     label="Data da próxima manutenção"
                     sx={{
-                      backgroundColor: "#192038",
+                      backgroundColor: '#192038',
                       borderRadius: 3,
-                      width: "33%",
-                      fontSize: "1rem",
+                      width: '33%',
+                      fontSize: '1rem',
                     }}
                   />
                 </ThemeProvider>
               </Box>
 
-              <Box sx={{ width: "100%", display: "flex", gap: 2, mb: 2 }}>
+              <Box sx={{ width: '100%', display: 'flex', gap: 2, mb: 2 }}>
                 <ThemeProvider theme={darkTheme}>
                   <TextField
                     sx={{
-                      backgroundColor: "#192038",
+                      backgroundColor: '#192038',
                       borderRadius: 3,
-                      width: "50%",
-                      fontSize: "1rem",
+                      width: '50%',
+                      fontSize: '1rem',
                     }}
                     id="empresa"
                     label="Empresa responsável"
@@ -258,10 +261,10 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                     value={empresa}
                     onChange={(e) => setEmpresa(e.target.value)}
                   />
-                  <FormControl sx={{ width: "50%" }}>
+                  <FormControl sx={{ width: '50%' }}>
                     <InputLabel
                       id="demo-simple-select-label"
-                      sx={{ color: "#FFFFFF" }}
+                      sx={{ color: '#FFFFFF' }}
                     >
                       Motoristas
                     </InputLabel>
@@ -272,8 +275,8 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
                       label="Motoristas"
                       onChange={(e) => setMotorista(e.target.value)}
                       sx={{
-                        color: "#FFFFFF",
-                        backgroundColor: "#192038",
+                        color: '#FFFFFF',
+                        backgroundColor: '#192038',
                       }}
                     >
                       {selectMotoristas.map((motorista) => (
@@ -289,17 +292,17 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
           </LocalizationProvider>
         }
         action={
-          <Box sx={{ width: "100%", display: "flex", gap: 2 }}>
+          <Box sx={{ width: '100%', display: 'flex', gap: 2 }}>
             <Button
               sx={{
-                textTransform: "none",
-                color: "red",
-                borderColor: "red",
-                width: "50%",
+                textTransform: 'none',
+                color: 'red',
+                borderColor: 'red',
+                width: '50%',
                 height: 40,
-                "&:hover": {
-                  color: "#e00000",
-                  border: "2px solid #e00000",
+                '&:hover': {
+                  color: '#e00000',
+                  border: '2px solid #e00000',
                 },
               }}
               variant="outlined"
@@ -311,14 +314,14 @@ const ModalEditVeiculo = ({ open, close, color, getVeiculos, data }) => {
 
             <Button
               sx={{
-                textTransform: "none",
-                color: "green",
-                borderColor: "green",
-                width: "50%",
+                textTransform: 'none',
+                color: 'green',
+                borderColor: 'green',
+                width: '50%',
                 height: 40,
-                "&:hover": {
-                  color: "#00c500",
-                  border: "2px solid #00c500",
+                '&:hover': {
+                  color: '#00c500',
+                  border: '2px solid #00c500',
                 },
               }}
               variant="outlined"
--- src/Components/Web/Pages/ControleFrotas/Historico.js
+++ src/Components/Web/Pages/ControleFrotas/Historico.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 import {
   Box,
   Button,
@@ -6,44 +6,44 @@ import {
   IconButton,
   Tooltip,
   Typography,
-} from "@mui/material";
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css";
-import AltRouteIcon from "@mui/icons-material/AltRoute";
-import { useLocation, useNavigate } from "react-router-dom";
-import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
-import ModalCadastroRotas from "../../Components/Modal/ModalCadastroRotas";
-import ModalRotas from "../../Components/Modal/ModalRotas";
-import Grid from "../../Components/Grid/Grid";
-import { GET_ROTAS } from "../../../../api";
-import CircleIcon from "@mui/icons-material/Circle";
-import AddRoadIcon from "@mui/icons-material/AddRoad";
-import dayjs from "dayjs";
-import ModalPdfXlsx from "../../Components/Modal/ModalPdfXlsx";
-import MapComponent from "../../Components/Maps/Teste";
+} from '@mui/material';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import AltRouteIcon from '@mui/icons-material/AltRoute';
+import { useLocation, useNavigate } from 'react-router-dom';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import ModalCadastroRotas from '../../Components/Modal/ModalCadastroRotas';
+import ModalRotas from '../../Components/Modal/ModalRotas';
+import Grid from '../../Components/Grid/Grid';
+import { GET_ROTAS } from '../../../../api';
+import CircleIcon from '@mui/icons-material/Circle';
+import AddRoadIcon from '@mui/icons-material/AddRoad';
+import dayjs from 'dayjs';
+import ModalPdfXlsx from '../../Components/Modal/ModalPdfXlsx';
+import MapComponent from '../../Components/Maps/Teste';
 
 const Historico = () => {
   const columns = [
     {
-      field: "cod_rota",
-      headerName: "CÓD ROTA",
+      field: 'cod_rota',
+      headerName: 'CÓD ROTA',
       flex: 0.6,
-      cellStyle: { textAlign: "center" },
-      headerClass: "header-center",
+      cellStyle: { textAlign: 'center' },
+      headerClass: 'header-center',
     },
     {
-      field: "status",
-      headerName: "STATUS",
+      field: 'status',
+      headerName: 'STATUS',
       flex: 0.4,
       cellRenderer: ({ data }) => {
         const statusConfig = (status) => {
           switch (status) {
-            case "aprovado":
-              return { color: "#03ef55", tooltip: "Rota Aprovada" };
-            case "reprovado":
-              return { color: "#ff3d71", tooltip: "Rota Reprovada" };
+            case 'aprovado':
+              return { color: '#03ef55', tooltip: 'Rota Aprovada' };
+            case 'reprovado':
+              return { color: '#ff3d71', tooltip: 'Rota Reprovada' };
             default:
-              return { color: "#ffc107", tooltip: "Rota Não Verificada" };
+              return { color: '#ffc107', tooltip: 'Rota Não Verificada' };
           }
         };
 
@@ -66,33 +66,33 @@ const Historico = () => {
       },
     },
     {
-      field: "partida.data_hora",
-      headerName: "DATA INÍCIO",
+      field: 'partida.data_hora',
+      headerName: 'DATA INÍCIO',
       flex: 1,
       valueFormatter: (params) => {
-        return dayjs(params.value).format("DD/MM/YYYY - HH:mm");
+        return dayjs(params.value).format('DD/MM/YYYY - HH:mm');
       },
     },
-    { field: "partida.cidade", headerName: "CIDADE ORIGEM", flex: 1 },
-    { field: "partida.estado", headerName: "UF", flex: 0.3 },
+    { field: 'partida.cidade', headerName: 'CIDADE ORIGEM', flex: 1 },
+    { field: 'partida.estado', headerName: 'UF', flex: 0.3 },
     {
-      field: "chegada.data_hora",
-      headerName: "DATA TÉRMINO",
+      field: 'chegada.data_hora',
+      headerName: 'DATA TÉRMINO',
       flex: 1,
       valueFormatter: (params) => {
-        return dayjs(params.value).format("DD/MM/YYYY - HH:mm");
+        return dayjs(params.value).format('DD/MM/YYYY - HH:mm');
       },
     },
-    { field: "chegada.cidade", headerName: "CIDADE DESTINO", flex: 1 },
-    { field: "chegada.estado", headerName: "UF", flex: 0.3 },
+    { field: 'chegada.cidade', headerName: 'CIDADE DESTINO', flex: 1 },
+    { field: 'chegada.estado', headerName: 'UF', flex: 0.3 },
     {
-      field: "tempo_gasto",
-      headerName: "TEMPO GASTO",
+      field: 'tempo_gasto',
+      headerName: 'TEMPO GASTO',
       flex: 1,
       valueGetter: (params) => {
         const inicio = dayjs(params.data.partida.data_hora);
         const chegada = dayjs(params.data.chegada.data_hora);
-        return chegada.diff(inicio, "minute");
+        return chegada.diff(inicio, 'minute');
       },
       valueFormatter: (params) => {
         const hours = Math.floor(params.value / 60);
@@ -101,12 +101,12 @@ const Historico = () => {
       },
     },
     {
-      field: "rota",
-      headerName: "OBS ROTA",
+      field: 'rota',
+      headerName: 'OBS ROTA',
       flex: 1,
       cellRenderer: ({ data }) => (
         <Button
-          sx={{ border: "1px solid #FFAA00", width: "50%" }}
+          sx={{ border: '1px solid #FFAA00', width: '50%' }}
           onClick={() => {
             setSelectedRota({ ...data, veiculo });
             setOpenRotas(true);
@@ -114,7 +114,7 @@ const Historico = () => {
         >
           <IconButton
             size="large"
-            sx={{ p: 0, width: "100%", color: "#FFAA00" }}
+            sx={{ p: 0, width: '100%', color: '#FFAA00' }}
           >
             <AltRouteIcon fontSize="small" />
           </IconButton>
@@ -129,6 +129,7 @@ const Historico = () => {
   const [openRotas, setOpenRotas] = useState(false);
   const [openCadastro, setOpenCadastro] = useState(false);
   const [openRelatorio, setOpenRelatorio] = useState(false);
+  const [openInfoRota, setOpenInfoRota] = useState(false);
   const [rows, setRows] = useState(false);
   const [selectedRota, setSelectedRota] = useState(null);
   const closeRotas = () => setOpenRotas(false);
@@ -147,6 +148,9 @@ const Historico = () => {
   const handleCloseRelatorio = () => {
     setOpenRelatorio(false);
   };
+  const handleCloseInfoRota = () => {
+    setOpenInfoRota(false);
+  };
 
   const getRotas = async () => {
     const { url, options } = GET_ROTAS(veiculo.cod_veiculo);
@@ -156,10 +160,10 @@ const Historico = () => {
       if (response.ok) {
         setRows(json);
       } else {
-        console.log("Erro ao buscar veículos");
+        console.log('Erro ao buscar veículos');
       }
     } catch (error) {
-      console.error("Erro na requisição:", error);
+      console.error('Erro na requisição:', error);
     }
   };
 
@@ -167,45 +171,50 @@ const Historico = () => {
     getRotas();
   }, []);
 
+  const onRowClick = (event) => {
+    console.log('aa');
+    setSelectedRota(event.data);
+    setOpenInfoRota(true);
+  };
   return (
     <>
       <Box
         sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
           mb: 2,
         }}
       >
         <Typography
           sx={{
-            color: "#FFFFFF",
-            textTransform: "uppercase",
-            fontSize: "2rem",
-            fontWeight: "bold",
+            color: '#FFFFFF',
+            textTransform: 'uppercase',
+            fontSize: '2rem',
+            fontWeight: 'bold',
           }}
         >
-          {veiculo ? `${veiculo.modelo} / ${veiculo.placa}` : "Veículo"}{" "}
+          {veiculo ? `${veiculo.modelo} / ${veiculo.placa}` : 'Veículo'}{' '}
         </Typography>
         <Box
           sx={{
-            display: "flex",
+            display: 'flex',
             gap: 2,
-            width: "50%",
-            justifyContent: "flex-end",
+            width: '50%',
+            justifyContent: 'flex-end',
           }}
         >
           <Button
             sx={{
-              textTransform: "none",
-              color: "#3366FF",
-              borderColor: "#3366FF",
-              width: "35%",
+              textTransform: 'none',
+              color: '#3366FF',
+              borderColor: '#3366FF',
+              width: '35%',
               height: 40,
-              "&:hover": {
-                color: "#FFFFFF",
-                border: "2px solid #FFFFFF",
+              '&:hover': {
+                color: '#FFFFFF',
+                border: '2px solid #FFFFFF',
               },
             }}
             variant="outlined"
@@ -216,14 +225,14 @@ const Historico = () => {
           </Button>
           <Button
             sx={{
-              textTransform: "none",
-              color: "#3366FF",
-              borderColor: "#3366FF",
-              width: "35%",
+              textTransform: 'none',
+              color: '#3366FF',
+              borderColor: '#3366FF',
+              width: '35%',
               height: 40,
-              "&:hover": {
-                color: "#FFFFFF",
-                border: "2px solid #FFFFFF",
+              '&:hover': {
+                color: '#FFFFFF',
+                border: '2px solid #FFFFFF',
               },
             }}
             variant="outlined"
@@ -241,8 +250,13 @@ const Historico = () => {
         getRotas={getRotas}
         veiculo={veiculo}
       />
-      <Box sx={{ height: 670, width: "100%", color: "white" }}>
-        <Grid ref={gridRef} columns={columns} rows={rows} />
+      <Box sx={{ height: 670, width: '100%', color: 'white' }}>
+        <Grid
+          ref={gridRef}
+          columns={columns}
+          rows={rows}
+          onRowClick={onRowClick}
+        />
       </Box>
       {/* <MapComponent /> */}
       <ModalRotas
